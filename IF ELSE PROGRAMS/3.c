33dca38a814dba014cd799190feaf2c11c563dda
SONALI RAWAT <rsona2628@gmail.com>
2025-11-03 06:50:56 +0530

    Find the Largest of two numbers

---
+++ IF ELSE PROGRAMS/3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int num1,num2;
+    printf("Enter first number:");
+    scanf("%d",&num1);
+    printf("Enter second number:");
+    scanf("%d",&num2);
+    if(num1>num2)
+    printf("%d is greater than %d",num1,num2);
+    else
+    printf("%d is greater than %d",num2,num1);
+
+    getch();
+}
